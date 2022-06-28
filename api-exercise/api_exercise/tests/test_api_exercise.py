e2f6214f380794a3adee0d467503062c09ddd9b1
Ngọc Hoàng Minh Hồ <hongochoangm@gmail.com>
2022-06-28 16:49:50 +0700

    [Task] Add feature test api with unittest

--- api-exercise/api_exercise/tests/test_api_exercise.py
+++ api-exercise/api_exercise/tests/test_api_exercise.py
@@ -1,5 +1,38 @@
-from api_exercise import __version__
+import io
+import os
+import unittest
+from fastapi.testclient import TestClient
 
+from fastapi import FastAPI
+from routes import predict_routes
 
-def test_version():
-    assert __version__ == '0.1.0'
+app = FastAPI()
+app.include_router(predict_routes)
+
+class UploadTest(unittest.TestCase):
+    client = TestClient(app)
+
+    def test_predict_history(self):
+        res = self.client.get('/')
+        assert res.status_code == 200
+
+    def test_predict_image(self):
+        foldername = 'missing'
+        for filename in os.listdir(foldername):
+            filepath = os.path.join(foldername,filename)
+            try:
+                with open(filepath, "rb") as file:
+                    res = self.client.post(
+                        "/", 
+                        files={
+                            "file": (
+                                filename, 
+                                file, 
+                                "image/jpeg"
+                            )
+                        }
+                    )
+                    assert res.status_code == 200
+                    print(res.text)
+            except Exception as e:
+                print("Error: %s" % e)
