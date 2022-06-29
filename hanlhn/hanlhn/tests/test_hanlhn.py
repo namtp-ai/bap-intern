5db32eeaa234525b0dd1f4cf095e94d8647cb236
hanahh080601 <hanlehngoc080601@gmail.com>
2022-06-29 22:55:49 +0700

    [Task] Write unittest for GET & POST methods

--- hanlhn/hanlhn/tests/test_hanlhn.py
+++ hanlhn/hanlhn/tests/test_hanlhn.py
@@ -1,5 +1,34 @@
-from hanlhn import __version__
+import unittest
+from fastapi.testclient import TestClient
 
+from fastapi import FastAPI
+from routes.flower import flower
 
-def test_version():
-    assert __version__ == '0.1.0'
+app = FastAPI()
+app.include_router(flower)
+
+class UnitTest(unittest.TestCase):
+    client = TestClient(app)
+
+    def test_get_list(self):
+        res = self.client.get('/')
+        assert res.status_code == 200
+
+    def test_predict_flower(self):
+        filepath = 'Rose_test.jpg'
+        try:
+            with open(filepath, "rb") as file:
+                res = self.client.post(
+                    "/", 
+                    files={
+                        "file": ( 
+                            filepath,
+                            file, 
+                            "image/jpg"
+                        )
+                    }
+                )
+                assert res.status_code == 200
+                print(res.text)
+        except Exception as e:
+            print("Here error: ", e)
