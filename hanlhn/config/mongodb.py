4c539cb9866463d3d0305d188fb1d5d49547322d
hanahh080601 <hanlehngoc080601@gmail.com>
2022-06-29 09:25:16 +0700

    [Task] Connect to mongodb atlas

---
+++ hanlhn/config/mongodb.py
@@ -0,0 +1,3 @@
+from pymongo import MongoClient
+
+conn = MongoClient("mongodb+srv://hana:1234@cluster0.7awqr.mongodb.net/hanlhn?authSource=admin&replicaSet=atlas-c0jjq4-shard-0&readPreference=primary&ssl=true")
